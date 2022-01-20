03195f1032a2395faa120eb0b0b1068a4e1ee75c
JohnPaul-Londono <johnplondono@gmail.com>
2022-01-20 13:45:00 -0600

    ternary update

--- src/views/Starwars.js
+++ src/views/Starwars.js
@@ -4,20 +4,38 @@ import axios from 'axios';
 
 const Starwars = (props) => {
     const { people_planets, id } = useParams();
-    const [starwars, setStarwars] = useState({});
+    const [starwars, setStarwars] = useState([]);
 
     useEffect(() => {
         axios.get("https://swapi.dev/api/" + people_planets + "/" + id)
-        .then(res => {
-            console.log(res);
-            setStarwars(res.data);
-        })
-        .catch (err => console.log(err))
+            .then(res => {
+                console.log(res);
+                setStarwars(res.data);
+            })
+            .catch(err => console.log(err))
     }, [people_planets, id])
 
     return (
-        <div className="search">
-            {starwars.name}
+        <div>
+            {
+                people_planets === "people" ?
+
+                    <div className="search">
+                        <h1>{starwars.name}</h1>
+                        <p> Height: {starwars.height} cm</p>
+                        <p>Ma$$: {starwars.mass} kg</p>
+                        <p>Hair Color: {starwars.hair_color}</p>
+                        <p>Skin Color: {starwars.skin_color}</p>
+                    </div>
+                    :
+                    <div className="search">
+                        <h1>{starwars.name}</h1>
+                        <p> Climate: {starwars.climate}  ø</p>
+                        <p>Population: {starwars.population} </p>
+                        <p>Terrain: {starwars.terrain}</p>
+                        <p>diameter: {starwars.diameter} </p>
+                    </div>
+            }
         </div>
     )
 }
